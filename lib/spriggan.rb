e5f2016a9dafe97d5e06e54b804104200bc8b78b
Peter Pan <peter@never.lan>
2024-05-07 06:32:54 -0400

    abstract everything away from beanstalk, increment minor vers:

--- lib/spriggan.rb
+++ lib/spriggan.rb
@@ -1,67 +1,88 @@
+require "./check_conn.rb"
+require "./bean_msg.rb"
 require "beaneater"
-require "timeout"
-require "socket"
 
-def port_open?(ip, port)
-  Timeout::timeout(2) do
-    begin
-      TCPSocket.new(ip, port).close
-      true
-    rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH, SocketError
-      false
-    rescue Timeout::Error
-      false
-    end #begin
-  end #do
-end #def
 
 class Spriggan
+  attr_accessor :core_threads
+  # default handler for any messages received through
+  # beanstalkd. Proc must be able to handle hash in 
+  # the bean_msg format.
   def initialize(
     beanstalk_host: "127.0.0.1",
     beanstalk_port: 11300,
-    module_name: "anonymous"
+    module_name: "default"
   )
     @core_threads = []
     @module_name = module_name
+    @bean_msg = BeanMsg.new(@module_name)
     @beanstalk_host = beanstalk_host
     @beanstalk_port = beanstalk_port
-    if port_open?(beanstalk_host, beanstalk_port)
-      @beanstalk = Beaneater.new("#{@beanstalk_host}\:#{@beanstalk_port}")
-    else
-      @beanstalk = nil
-    end
+    @beanstalk = Beaneater.new("#{@beanstalk_host}\:#{@beanstalk_port}")
   end #def
 
   # Logs a message to stdout, with flush to work with PM2
-  def log(msg)
+  def pm2_log(msg)
     $stdout.puts msg
     $stdout.flush
   end
 
+  # Send a message to beanstalkd with a priority of 100 (default is 0,
+  # which is also the highest pri) delay of 0, and ttr of 300. 
+  # This will auto-delete the job after 300 seconds.
   def seng_msg(obj, tube)
     bean = @beanstalk.tubes[tube]
-    msg = obj.to_yaml
-    msg64 = Base64.encode64(msg)
-    bean.put msg64
+    str = @bean_msg.wrap_msg(obj, tube)
+    bean.put str, :pri => 100, :delay => 0, :ttr => 300
+    pm2_log "Sent message: #{str}"
+  end
+  
+  # blocks until a job is sent to the module's beanstalk tube, then
+  # decodes it and returns a bean_msg formatted hash.
+  def get_msg
+    @beanstalk.tubes.watch!(@module_name)
+    job = @beanstalk.tubes.reserve # this will block until a job is received
+    msg_hash = open_msg(job.body)
+    pm2_log "Received job: #{msg_hash['msg']}; from: #{msg_hash['from']};"
+    job.delete
+    return msg_hash
   end
 
-  def open_msg(msg64)
-    msg = Base64.decode64(msg64)
-    obj = YAML.load(msg)
-    return obj
-  end
-
+  # Adds the given block to the core_threads array, and puts it
+  # immediately to sleep
   def add_thread(&block)
+    puts "adding thread..."
     thr = Thread.new { Thread.stop; block.call }
     @core_threads << thr
   end
 
+  # Run all the threads, with a trap for signal interrupt from
+  # PM2, and join in case of more than one thread.
   def run
-    @beanstalk.tubes.find(@module_name) # also creates the tube
-    @beanstalk.tubes.watch!(@module_name)
+    Signal.trap("INT") {
+      i = 0
+      core_threads.each { |t|
+      pm2_log "killing core thread #{i}.."
+        t.kill
+        i += 1
+      }
+      pm2_log "Exiting gracefully."
+      exit
+    }
     @core_threads.each { |thr| thr.wakeup }
-    @core_threads.each { |thr| thr.join }
+    @core_threads.each { |thr| thr.run }
+    if @core_threads.count > 1
+      @core_threads.each { |thr| thr.join }
+    end #if
+    @core_threads = []
   end #def
 
   private #===========// PRIVATE METHODS //===========//
+
+  # converts a string into a formatted message hash, also decoding
+  # the base64 message string in the hash.
+  def open_msg(str)
+    msg_hash = @bean_msg.open_msg(str)
+    return msg_hash
+  end
 end
--- test/function_tests.rb
+++ test/function_tests.rb
@@ -2,9 +2,42 @@ require "spriggan"
 require "test/unit"
 
 class TestSprig < Test::Unit::TestCase
-  def test_log
+  def test_initialization
     sp = Spriggan.new
     o = sp.log("it was Jack")
     assert_equal(o, $stdout)
+  end
+
+  def test_messages
+    i = 5
+    sp = Spriggan.new(module_name: "test_messages")
+    sp.seng_msg(i, "test_messages")
+    msg_hash = sp.get_msg
+    assert_equal("test_messages", msg_hash["to"])
+    assert_equal("test_messages", msg_hash["from"])
+    assert_equal(i, msg_hash["msg"])
+  end
+
+  def run_one_thread
+    sp = Spriggan.new
+    i = 2
+    sp.add_thread {
+      i+=1
+    }
+    sp.run
+    assert_equal(i, 3)
+  end
+
+  def run_two_threads
+    sp = Spriggan.new
+    i = 1
+    sp.add_thread {
+      i+=1
+    }
+    sp.add_thread {
+      i+=3
+    }
+    sp.run
+    assert_equal(i, 5)
   end
 end
